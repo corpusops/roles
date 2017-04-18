3eb397f31c86f5c83e3a704f59d240b1b7980065
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-04-18 23:01:31 +0200

    Importing lxc_create

---
+++ lxc_create/templates/cops_lxc_prereqs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+cd $(dirname $0)
+container=$(echo $(basename $(dirname $(readlink -f $0))))
+lxcap=$(echo $(dirname $(readlink -f $0)))
+lxcp=$(echo $(dirname $(dirname $(readlink -f $0))))
+for i in \
+    bin/cops_shell_common \
+    bin/cops_pkgmgr_install.sh \
+    bin/cops_reset-host.py \
+    bin/cops_lxc_init.sh;do
+    if [ -e $i ];then
+        echo "copying $i" >&2
+        ( cat $i | lxc-attach -P "$lxcp" -n $container -- tee /$i ) >/dev/null
+        lxc-attach -P "$lxcp" -n $container -- chmod +x /$i
+    fi
+done
+echo "Running /bin/cops_lxc_init.sh in $container ($lxcp)" >&2
+lxc-attach -P "${lxcp}" -n $container -- bash /bin/cops_lxc_init.sh
+# vim:set et sts=4 ts=4 tw=80:
