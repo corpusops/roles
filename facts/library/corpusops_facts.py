d3409fc18df8ffe7b58f2adcd34da1bfbc86b766
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2018-03-12 17:04:08 +0100

    unused imports (dont have six on baremetal)

--- facts/library/corpusops_facts.py
+++ facts/library/corpusops_facts.py
@@ -4,12 +4,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import copy
-import six
 import contextlib
 import socket
-import re
-import json
 import urllib2
 import os
 
