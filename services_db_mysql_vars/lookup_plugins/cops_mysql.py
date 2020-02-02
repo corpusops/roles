b2e036f7bad697d52db33b4ff57bd5e20559069e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-02-02 21:34:35 +0100

    Registries refactor: services_db_mysql_vars

---
+++ services_db_mysql_vars/lookup_plugins/cops_mysql.py
@@ -0,0 +1,66 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = """
+    lookup: cops_mysql
+    author: kiorky
+    version_added: "2.5"
+    short_description: wrapper for copsf_mysql filter in lookup context
+    description:
+    - wrapper for copsf_mysql filter in lookup context
+    options:
+      _terms:
+        description: The strings to render
+        required: True
+      default:
+        description:
+            - What to return if a variable is undefined.
+            - If no default is set, it will result in an error if any of the variables is undefined.
+"""
+
+EXAMPLES = """
+"""
+
+RETURN = """
+_value:
+  description:
+    - value of the variables requested.
+"""
+
+from ansible.errors import AnsibleError, AnsibleUndefinedVariable
+from ansible.module_utils.six import string_types
+from ansible.plugins.lookup import LookupBase
+import six
+
+from copsf_mysql import __funcs__
+
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, func='copsf_mysql', **kwargs):
+        if variables is not None:
+            self._templar.available_variables = variables
+
+        self.set_options(direct=kwargs)
+        default = self.get_option('default')
+
+        ret = []
+        for value in terms:
+            try:
+                ret.append(
+                    self._templar.template(
+                    self._templar.template(
+                        __funcs__['{0}'.format(func)](
+                            prefix=value,
+                            ansible_vars=self._templar.available_variables, **kwargs),
+                    fail_on_undefined=True))
+                )
+            except AnsibleUndefinedVariable:
+                if default is not None:
+                    ret.append(default)
+                else:
+                    raise
+
+        return ret
+
+# vim:set et sts=4 ts=4 tw=80
