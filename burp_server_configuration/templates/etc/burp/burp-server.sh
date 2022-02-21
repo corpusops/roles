08aecf485e4f8af631971de7f0634279af32acdb
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2022-02-21 21:53:42 +0100

    burp server admin wrapper scripts

--- burp_server_configuration/templates/etc/burp/burp-server.sh
+++ burp_server_configuration/templates/etc/burp/burp-server.sh
@@ -2,12 +2,11 @@
 set -e
 W=$(cd "$(dirname "$(readlink -f "$0")")" && pwd)
 SSLCONF="$W/openssl.cnf"
-BURP_RESTORE_CONFIG="${BURP_RESTORE_CONFIG-$W/burp-client-restore.conf}"
 args=""
+BURP_CONFIG="${BURP_CONFIG-$W/burp-server-admin.conf}"
 if [[ -n ${DEBUG-} ]];then set -x;fi
-if [ -e "$BURP_RESTORE_CONFIG" ];then args="-c";fi
+if [ -e $BURP_CONFIG ];then args="-c";fi
 if [ -e "$SSLCONF" ];then
     export OPENSSL_CONF="$SSLCONF"
 fi
-exec burp $args $BURP_RESTORE_CONFIG "$@"
-# vim:set et sts=4 ts=4 tw=0:
+exec burp $args $BURP_CONFIG "$@"
